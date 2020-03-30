0649167edff76340d7463174c586ad5a8dfa1a12
izelacerda <izelacerdajl@gmail.com>
2020-03-30 16:34:08 -0300

    update  eliminar comentarios

--- backfastfeet/src/database/index.js
+++ backfastfeet/src/database/index.js
@@ -1,5 +1,4 @@
 import Sequelize from 'sequelize';
-// import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -14,7 +13,6 @@ const models = [User, File, Recipient, Order, OrderProblem];
 class Database {
   constructor() {
     this.init();
-    // this.mongo();
   }
 
   init() {
@@ -24,14 +22,6 @@ class Database {
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
   }
-
-  // mongo() {
-  //   this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
-  //     useNewUrlParser: true,
-  //     useUnifiedTopology: true,
-  //     useFindAndModify: true,
-  //   });
-  // }
 }
 
 export default new Database();
